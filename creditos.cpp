925bdcbbf1c4342bd0ac3fbb207170d6b11f13f1
Lic. Daysi Lopez Reyez <00073824@uca.edu.sv>
2024-06-24 11:01:33 -0600

    Primer avance Johnny-Arteaga

---
+++ creditos.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"\tFUNDAMENTOS DE PROGRAMACION\n\t\tCICLO 01\n\tCATEDRATICA ING. ELISA ALDANA\n\t\tJUEGO HECHO POR:\n\t\tJULIO FLORES\n\t\tJOHNNY ARTEAGA\n\t\tANDREA ALVAREZ";
+
+    return 0;
+}
