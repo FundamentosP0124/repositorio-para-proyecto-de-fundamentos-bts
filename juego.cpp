ab95755b87944ea055dc9e399c118785a6ffc348
Lic. Daysi Lopez Reyez <00073824@uca.edu.sv>
2024-06-28 18:48:10 -0600

    juego

--- juego.cpp
+++ juego.cpp
@@ -9,7 +9,38 @@ struct Usuario
 
 int main(){
 
-int eleccion_del_usuario_en_el_menu = 0, eleccion_del_modo_de_juego = 0, eleccion_del_tamanio_de_cuadricula = 0;
+int eleccion_del_usuario_en_el_menu = 0, eleccion_del_modo_de_juego = 0, eleccion_del_tamanio_de_cuadricula = 0, fila, columna, opcion;
+
+char juego_rapido_J1[4][4]={0};
+char juego_rapido_J2[4][4]={0};
+
+char juego_largo_J1[8][8]={0};
+char juego_largo_J2[8][8]={0};
+   
+int n1 = 1, n2 = 1;
+
+for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+         juego_rapido_J1[i][j] = '0';
+        }
+}
+for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+        juego_rapido_J2[i][j] = '0';
+    }
+}
+
+for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+        juego_largo_J1[i][j] = '0';
+    }
+}
+for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+        juego_largo_J2[i][j] = '0';
+    }
+}
+
 do
 {
 
@@ -47,7 +78,8 @@ switch (eleccion_del_usuario_en_el_menu) {
                             cout<<"Escribe un nombre para tu equipo: ";
                                 Usuario usuario1;
                                 cin>>usuario1.nombre;
-                                //Acá va la matriz de 4x4 
+                                
+                                                      
                             }
                             else if ((eleccion_del_modo_de_juego==2)&&(eleccion_del_tamanio_de_cuadricula==1)){
                             cout<<"Escribe el nombre del equipo del jugador 1: ";
@@ -58,10 +90,76 @@ switch (eleccion_del_usuario_en_el_menu) {
                                 Usuario usuario2;
                                 cin>>usuario2.nombre;
                                 //Acá van las matrices de 4x4 
-                            }
+                                
+                                cout << "JUGADOR 1, " << usuario1.nombre << ". El mapa original es:" << endl;
+                                for (int i = 0; i < 4; i++) {
+                                    for (int j = 0; j < 4; j++) {
+                            
+                                        cout <<" | " <<juego_rapido_J1[i][j] << " | ";
+                                    }
+                                    cout << endl;
+                                }
+                                
+                                cout << "JUGADOR 1, " << usuario1.nombre << ". Ingrese  las posiciones de sus cuatro barcos: " << endl;
+                                for (int i = 0; i < 4; ++i) {
+                                    cout << "Posicion del barco " << n1++ << ": " << endl;
+                                    cout << "Fila: ";
+                                    cin >> fila;
+                                    cout << "Columna: ";
+                                    cin >> columna;
+                                    if (fila >= 0 && fila < 4 && columna >= 0 && columna < 4) {
+                                        juego_rapido_J1[fila][columna] = 'x';
+                                    } else {
+                                        cout << "Posición inválida. Intente de nuevo la ultima posicion." << endl;
+                                        --i;
+                                }
+                                }
+                                
+                                //Mapa resultante jugador 1
+                                cout << usuario1.nombre << ", este es su nuevo mapa (x = barco):" << endl;
+                                for (int i = 0; i < 4; ++i) {
+                                    for (int j = 0; j < 4; ++j) {
+                                        cout <<" | " <<juego_rapido_J1[i][j] << " | ";
+                                    }
+                                    cout << endl;
+                                }
+
+                                
+                                cout << "JUGADOR 2 " << usuario2.nombre << ". El mapa original es:" << endl;
+                                for (int i = 0; i < 4; i++) {
+                                    for (int j = 0; j < 4; j++) {
+                            
+                                        cout <<" | " <<juego_rapido_J2[i][j] << " | ";
+                                    }
+                                    cout << endl;
+                                }
+                                //Ingreso de las posiciones del jugador
+                                cout << "JUGADOR 1 " << usuario2.nombre << ". Ingrese  las posiciones de sus cuatro barcos: " << endl;
+                                for (int i = 0; i < 4; ++i) {
+                                    cout << "Posicion del barco " << n2++ << ": " << endl;
+                                    cout << "Fila: ";
+                                    cin >> fila;
+                                    cout << "Columna: ";
+                                    cin >> columna;
+                                    if (fila >= 0 && fila < 4 && columna >= 0 && columna < 4) {
+                                        juego_rapido_J2[fila][columna] = 'x';
+                                    } else {
+                                        cout << "Posición inválida. Intente de nuevo la ultima posicion." << endl;
+                                        --i;
+                                }
+                                }
+                                
+                                //Mapa resultante
+                                cout << usuario2.nombre << ". Este es su nuevo mapa (x = barco):" << endl;
+                                for (int i = 0; i< 4; ++i) {
+                                    for (int j = 0; j < 4; ++j) {
+                                        cout <<" | " <<juego_rapido_J2[i][j] << " | ";
+                                    }
+                                    cout << endl;
+                                }
 
 
-                            else if ((eleccion_del_modo_de_juego==1)&&(eleccion_del_tamanio_de_cuadricula==2)){
+                            }else if ((eleccion_del_modo_de_juego==1)&&(eleccion_del_tamanio_de_cuadricula==2)){
                             cout<<"Escribe el nombre del equipo del jugador 1: ";
                                 Usuario usuario1;
                                 cin>>usuario1.nombre;
@@ -82,7 +180,77 @@ switch (eleccion_del_usuario_en_el_menu) {
                                 Usuario usuario2;
                                 cin>>usuario2.nombre;
                                 //Acá van las matrices de 8x8 
+
+                            
+                            cout << "JUGADOR 1, " << usuario1.nombre << ". El mapa original es:" << endl;
+                            for (int i = 0; i < 8; i++) {
+                                for (int j = 0; j < 8; j++) {
+                                    
+                                    cout <<" | " <<juego_largo_J1[i][j] << " | ";
+                                }
+                                cout << endl;
                             }
+                            
+                            //Ingreso de las posiciones del jugador 1
+                            cout << "JUGADOR 1, " << usuario1.nombre << ". Ingrese  las posiciones de sus cuatro barcos: " << endl;
+                            for (int i = 0; i < 8; ++i) {
+                                cout << "Posicion del barco " << n1++ << ": " << endl;
+                                cout << "Fila: ";
+                                cin >> fila;
+                                cout << "Columna: ";
+                                cin >> columna;
+                                if (fila >= 0 && fila < 8 && columna >= 0 && columna < 8) {
+                                    juego_largo_J1[fila][columna] = 'x';
+                                } else {
+                                    cout << "Posición inválida. Intente de nuevo la ultima posicion." << endl;
+                                    --i;
+                            }
+                            }
+                            
+                            //Mapa resultante jugador 1
+                            cout << usuario1.nombre << ". Este es su nuevo mapa (x = barco):" << endl;
+                            for (int i = 0; i < 8; ++i) {
+                                for (int j = 0; j < 8; ++j) {
+                                    cout <<" | " <<juego_largo_J1[i][j] << " | ";
+                                }
+                            }
+
+                             //Mapa original jugador 2
+                            cout << "JUGADOR 2, " << usuario2.nombre << ". El mapa original es:" << endl;
+                            for (int i = 0; i < 8; i++) {
+                                for (int j = 0; j < 8; j++) {
+                                    
+                                    cout <<" | " <<juego_largo_J2[i][j] << " | ";
+                                }
+                                cout << endl;
+                            }
+                            
+                            //Ingreso de las posiciones del jugador 2
+                            cout << "JUGADOR 2, " << usuario2.nombre << ". Ingrese  las posiciones de sus cuatro barcos: " << endl;
+                            for (int i = 0; i < 8; ++i) {
+                                cout << "Posicion del barco " << n2++ << ": " << endl;
+                                cout << "Fila: ";
+                                cin >> fila;
+                                cout << "Columna: ";
+                                cin >> columna;
+                                if (fila >= 0 && fila < 8 && columna >= 0 && columna < 8) {
+                                    juego_largo_J2[fila][columna] = 'x';
+                                } else {
+                                    cout << "Posición inválida. Intente de nuevo la ultima posicion." << endl;
+                                    --i;
+                            }
+                            }
+                            
+                            //Mapa resultante jugador 2
+                            cout << usuario2.nombre << ". Este es su nuevo mapa (x = barco):" << endl;
+                            for (int i = 0; i < 8; ++i) {
+                                for (int j = 0; j < 8; ++j) {
+                                    cout <<" | " <<juego_largo_J2[i][j] << " | ";
+                                }
+                                cout << endl;
+                            }
+                            }
+
 
                             if (eleccion_del_tamanio_de_cuadricula != 1 && eleccion_del_tamanio_de_cuadricula != 2) {
                                 cout << "Por favor escoja una opcion valida.\n\n";
